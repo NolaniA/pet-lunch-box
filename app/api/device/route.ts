bdead55816e637b0779ff02b4e583645bcf27b5a
Nolania <132760685+NolaniA@users.noreply.github.com>
2026-04-14 14:22:54 +0700

    add api get for device(esp32) and page sittings

--- app/api/device/route.ts
+++ app/api/device/route.ts
@@ -2,15 +2,32 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function GET(req: Request) {
   const supabase = await createClient();
-
   const { searchParams } = new URL(req.url);
-  const user_id = searchParams.get("user_id");
+  const token = searchParams.get("token");
 
-  const { data } = await supabase
+  console.log(token);
+
+  if (!token) {
+    return Response.json({ error: "no token" }, { status: 400 });
+  }
+
+  // 1. หา user_id
+  const { data: userData } = await supabase
+    .from("user_data")
+    .select("user_id")
+    .eq("device_token", token)
+    .single();
+
+  if (!userData) {
+    return Response.json({ error: "invalid token" }, { status: 401 });
+  }
+
+  // 2. ดึง schedule
+  const { data: schedule } = await supabase
     .from("feed_schedule")
     .select("hour, minute, duration_sec")
-    .eq("user_id", user_id)
+    .eq("user_id", userData.user_id)
     .order("hour");
 
-  return Response.json(data);
+  return Response.json(schedule);
 }
