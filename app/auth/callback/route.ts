a4a2654b9dad630d54f044e621d358de2d3c5a6e
Nolania <132760685+NolaniA@users.noreply.github.com>
2026-04-14 08:47:21 +0700

    edit method createClient

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,15 +1,14 @@
 // app/auth/callback/route.ts
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
 
   if (code) {
-    const supabase = createClient(await cookies());
-    await supabase.auth.exchangeCodeForSession(code); // 🔥 ตัวสำคัญ
+    const supabase = await createClient();
+    await supabase.auth.exchangeCodeForSession(code);
   }
 
   return NextResponse.redirect(`${origin}/`);
